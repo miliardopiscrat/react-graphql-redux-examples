456826084d37b1eb8bc9f084d72bc3179a1a9d39
Dariusz Górnik <dariusz.gornik@nokia.com>
2019-12-05 12:33:04 +0100

    add axios example

--- tutorial/apps/tutorial-react/src/app/app.tsx
+++ tutorial/apps/tutorial-react/src/app/app.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { GetBooks } from 'apps/tutorial-react/src/app/components/get-books-query';
 import { ReadBooksFromStore } from 'apps/tutorial-react/src/app/components/read-books-from-store';
 import { BorrowBooks } from 'apps/tutorial-react/src/app/components/borrow-book-mutation';
+import { AxiosExample } from 'apps/tutorial-react/src/app/components/axios-example';
 
 export const App = () => {
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <GetBooks/>
-        <ReadBooksFromStore/>
-        <BorrowBooks/>
+        <AxiosExample/>
       </div>
     </>
   );
---
+++ tutorial/apps/tutorial-react/src/app/components/axios-example.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useEffect, useState } from 'react';
+import axios from 'axios';
+
+type Book = {
+  volumeInfo: {
+    title: string;
+  }
+}
+
+type Books = {
+  items: Book[]
+}
+
+const output = axios.get<Books>('https://www.googleapis.com/books/v1/volumes?q=harry+potter');
+
+
+export const AxiosExample: FC = () => {
+  const [books, setBooks] = useState({} as Books);
+
+  useEffect(() => {
+    if (output) {
+      output.then((result) => {
+        setBooks(result.data);
+      });
+    }
+  }, []);
+
+  return (<>
+    <div style={{ 'display': 'grid', 'gridAutoFlow': 'row' }}>
+      {
+        books.items ? books.items.map(({volumeInfo: {title}}, key) =>
+        <span key={key}>{title}</span>): <span>LOADING...</span>
+      }
+    </div>
+  </>);
+};
